393186a6369639dc3571aea7fcb569e64a139d78
Bernard Maisel <ntjust26@gmail.com>
2016-09-01 18:32:03 +0900

    Added exercises for variables section

---
+++ ch3_variables/name.rb
@@ -0,0 +1,30 @@
+#Task 1
+
+
+puts "What's your name?"
+name = gets.chomp
+puts "Hey there " + name
+
+
+
+#Task 3
+
+10.times do
+  puts name
+end
+
+#Task 4
+
+puts "What is your first name?"
+f_name = gets.chomp
+puts "What is your last name?"
+l_name = gets.chomp
+puts "Hello " + f_name + " " + l_name
+
+
+#task 5
+#the first prints out the number 3.
+#the second one prints out 'undefined local variable or method' because the variable 'x' is defined inside the do/end loop.
+
+#task 6
+#a variable "shoes" is either missing/undefined or outside of the callable scope.
